0f3458fb190f196414b9abdc14bdf3cae631eb03
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-11-15 15:44:12 +0100

    Initial commit for Taxi service project

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,42 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Manufacturer, Car, Driver
-
-
-@admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'license_number']
-    fieldsets = (
-        (None, {
-            'fields': ('username', 'password')
-        }),
-        ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email')
-        }),
-        ('Additional info', {
-            'fields': ('license_number',)
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-        }),
-        ('Important dates', {
-            'fields': ('last_login', 'date_joined')
-        }),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'license_number'),
-        }),
-    )
 
 
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'country']
+    list_display = ("name", "country")
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ['model', 'manufacturer']
-    search_fields = ['model']
-    list_filter = ['manufacturer']
+    list_display = ("model", "manufacturer")
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    list_display = UserAdmin.list_display + ("license_number",)
